bb780fde991f8d347f098019310dcedde43a5120
Tomáš David <tomasdav9@gmail.com>
2022-12-14 06:52:48 +0000

    Filled mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Tomáš David`,
+    siteTitleAlt: `Tomáš David - Software Developer`,
+    siteHeadline: `Tomáš David - Software Developer`,
+    siteUrl: `https://tomdavid.cz`,
+    siteDescription: `Tomáš David - Software Developer`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@tomm_david`,
   },
   trailingSlash: `never`,
   plugins: [
